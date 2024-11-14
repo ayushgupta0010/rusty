ff5b81e67615424d0ad16d3b50c57f5566f65dd4
ayushgupta0010 <ayushgupta0010@gmail.com>
2024-11-14 01:24:13 -0500

    Tokenize numbers and identifiers

--- src/lib.rs
+++ src/lib.rs
@@ -161,7 +161,7 @@ impl<'de> Iterator for Lexer<'de> {
                 '"' => Started::String,
                 c if c.is_whitespace() => continue,
                 '0'..='9' => Started::Number,
-                'a'..='z' | '_' => Started::Ident,
+                'a'..='z' | 'A'..='Z' | '_' => Started::Ident,
                 c => {
                     return Some(Err(miette::miette!(
                         labels = vec![LabeledSpan::at(
@@ -176,8 +176,82 @@ impl<'de> Iterator for Lexer<'de> {
 
             break match started {
                 Started::String => todo!(),
-                Started::Number => todo!(),
-                Started::Ident => todo!(),
+                Started::Ident => {
+                    let first_non_ident = c_onwards
+                        .find(|c| !matches!(c, 'a'..='z' | 'A'..='Z' | '0'..='9' | '_'))
+                        .unwrap_or_else(|| c_onwards.len());
+
+                    let literal = &c_onwards[..first_non_ident];
+                    let extra_bytes = literal.len() - c.len_utf8();
+                    self.byte += extra_bytes;
+                    self.rest = &self.rest[extra_bytes..];
+
+                    let kind = match literal {
+                        "and" => TokenKind::And,
+                        "class" => TokenKind::Class,
+                        "else" => TokenKind::Else,
+                        "false" => TokenKind::False,
+                        "for" => TokenKind::For,
+                        "fun" => TokenKind::Fun,
+                        "if" => TokenKind::If,
+                        "nil" => TokenKind::Nil,
+                        "or" => TokenKind::Or,
+                        "return" => TokenKind::Return,
+                        "super" => TokenKind::Super,
+                        "this" => TokenKind::This,
+                        "true" => TokenKind::True,
+                        "var" => TokenKind::Var,
+                        "while" => TokenKind::While,
+                        _ => TokenKind::Ident,
+                    };
+
+                    return Some(Ok(Token {
+                        origin: literal,
+                        kind,
+                    }));
+                }
+                Started::Number => {
+                    let first_non_digit = c_onwards
+                        .find(|c| !matches!(c, '.' | '0'..='9'))
+                        .unwrap_or_else(|| c_onwards.len());
+
+                    let mut literal = &c_onwards[..first_non_digit];
+                    let mut dotted = literal.splitn(3, '.');
+                    match (dotted.next(), dotted.next(), dotted.next()) {
+                        (Some(one), Some(two), Some(_)) => {
+                            literal = &literal[..one.len() + 1 + two.len()]
+                        }
+                        (Some(one), Some(two), None) if two.is_empty() => {
+                            literal = &literal[..one.len()]
+                        }
+                        _ => {
+                            // leave literal as-is
+                        }
+                    }
+
+                    let extra_bytes = literal.len() - c.len_utf8();
+                    self.byte += extra_bytes;
+                    self.rest = &self.rest[extra_bytes..];
+
+                    let n = match literal.parse() {
+                        Ok(n) => n,
+                        Err(e) => {
+                            return Some(Err(miette::miette!(
+                                labels = vec![LabeledSpan::at(
+                                    self.byte - literal.len()..self.byte,
+                                    "this numeric literal"
+                                )],
+                                "{e}"
+                            )
+                            .with_source_code(self.whole.to_string())));
+                        }
+                    };
+
+                    return Some(Ok(Token {
+                        origin: literal,
+                        kind: TokenKind::Number(n),
+                    }));
+                }
                 Started::IfEqualElse(yes, no) => {
                     self.rest = self.rest.trim_start();
                     let trimmed = c_onwards.len() - self.rest.len() - 1;

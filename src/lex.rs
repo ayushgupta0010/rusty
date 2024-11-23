f111d5293d3172b598facc14fae03ef1e95408ff
ayushgupta0010 <ayushgupta0010@gmail.com>
2024-11-22 23:58:52 -0500

    Add Pratt Parser

--- src/lex.rs
+++ src/lex.rs
@@ -3,6 +3,10 @@ use std::{borrow::Cow, fmt};
 use thiserror::Error;
 
 #[derive(Diagnostic, Debug, Error)]
+#[error("Unexpected EOF")]
+pub struct Eof;
+
+#[derive(Diagnostic, Debug, Error)]
 #[error("Unexpected token '{token}'")]
 pub struct SingleTokenError {
     #[source_code]
@@ -16,8 +20,8 @@ pub struct SingleTokenError {
 
 impl SingleTokenError {
     pub fn line(&self) -> usize {
-        let until_recognized = &self.src[..=self.err_span.offset()];
-        until_recognized.lines().count()
+        let until_unrecongized = &self.src[..=self.err_span.offset()];
+        until_unrecongized.lines().count()
     }
 }
 
@@ -33,15 +37,16 @@ pub struct StringTerminationError {
 
 impl StringTerminationError {
     pub fn line(&self) -> usize {
-        let until_recognized = &self.src[..=self.err_span.offset()];
-        until_recognized.lines().count()
+        let until_unrecongized = &self.src[..=self.err_span.offset()];
+        until_unrecongized.lines().count()
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Token<'de> {
-    origin: &'de str,
-    kind: TokenKind,
+    pub origin: &'de str,
+    pub offset: usize,
+    pub kind: TokenKind,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq)]
@@ -56,7 +61,6 @@ pub enum TokenKind {
     Plus,
     Semicolon,
     Star,
-    Bang,
     BangEqual,
     EqualEqual,
     LessEqual,
@@ -64,6 +68,7 @@ pub enum TokenKind {
     Less,
     Greater,
     Slash,
+    Bang,
     Equal,
     String,
     Ident,
@@ -87,53 +92,53 @@ pub enum TokenKind {
 }
 
 impl fmt::Display for Token<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let origin = self.origin;
         match self.kind {
-            TokenKind::LeftParen => write!(f, "LEFT_PAREN"),
-            TokenKind::RightParen => write!(f, "RIGHT_PAREN"),
-            TokenKind::LeftBrace => write!(f, "LEFT_BRACE"),
-            TokenKind::RightBrace => write!(f, "RIGHT_BRACE"),
-            TokenKind::Comma => write!(f, "COMMA"),
-            TokenKind::Dot => write!(f, "DOT"),
-            TokenKind::Minus => write!(f, "MINUS"),
-            TokenKind::Plus => write!(f, "PLUS"),
-            TokenKind::Semicolon => write!(f, "SEMICOLON"),
-            TokenKind::Star => write!(f, "STAR"),
-            TokenKind::BangEqual => write!(f, "BANG_EQUAL"),
-            TokenKind::EqualEqual => write!(f, "EQUAL_EQUAL"),
-            TokenKind::LessEqual => write!(f, "LESS_EQUAL"),
-            TokenKind::GreaterEqual => write!(f, "GREATER_EQUAL"),
-            TokenKind::Less => write!(f, "LESS"),
-            TokenKind::Greater => write!(f, "GREATER"),
-            TokenKind::Slash => write!(f, "SLASH"),
-            TokenKind::Bang => write!(f, "BANG"),
-            TokenKind::Equal => write!(f, "EQUAL"),
+            TokenKind::LeftParen => write!(f, "LEFT_PAREN {origin} null"),
+            TokenKind::RightParen => write!(f, "RIGHT_PAREN {origin} null"),
+            TokenKind::LeftBrace => write!(f, "LEFT_BRACE {origin} null"),
+            TokenKind::RightBrace => write!(f, "RIGHT_BRACE {origin} null"),
+            TokenKind::Comma => write!(f, "COMMA {origin} null"),
+            TokenKind::Dot => write!(f, "DOT {origin} null"),
+            TokenKind::Minus => write!(f, "MINUS {origin} null"),
+            TokenKind::Plus => write!(f, "PLUS {origin} null"),
+            TokenKind::Semicolon => write!(f, "SEMICOLON {origin} null"),
+            TokenKind::Star => write!(f, "STAR {origin} null"),
+            TokenKind::BangEqual => write!(f, "BANG_EQUAL {origin} null"),
+            TokenKind::EqualEqual => write!(f, "EQUAL_EQUAL {origin} null"),
+            TokenKind::LessEqual => write!(f, "LESS_EQUAL {origin} null"),
+            TokenKind::GreaterEqual => write!(f, "GREATER_EQUAL {origin} null"),
+            TokenKind::Less => write!(f, "LESS {origin} null"),
+            TokenKind::Greater => write!(f, "GREATER {origin} null"),
+            TokenKind::Slash => write!(f, "SLASH {origin} null"),
+            TokenKind::Bang => write!(f, "BANG {origin} null"),
+            TokenKind::Equal => write!(f, "EQUAL {origin} null"),
             TokenKind::String => write!(f, "STRING {origin} {}", Token::unescape(origin)),
-            TokenKind::Ident => write!(f, "IDENTFIER {origin}"),
+            TokenKind::Ident => write!(f, "IDENTIFIER {origin} null"),
             TokenKind::Number(n) => {
                 if n == n.trunc() {
-                    write!(f, "NUMBER {n}.0")
+                    write!(f, "NUMBER {origin} {n}.0")
                 } else {
-                    write!(f, "NUMBER {n}")
+                    write!(f, "NUMBER {origin} {n}")
                 }
             }
-            TokenKind::And => write!(f, "AND"),
-            TokenKind::Class => write!(f, "CLASS"),
-            TokenKind::Else => write!(f, "ELSE"),
-            TokenKind::False => write!(f, "FALSE"),
-            TokenKind::For => write!(f, "FOR"),
-            TokenKind::Fun => write!(f, "FUN"),
-            TokenKind::If => write!(f, "IF"),
-            TokenKind::Nil => write!(f, "NIL"),
-            TokenKind::Or => write!(f, "OR"),
-            TokenKind::Print => write!(f, "PRINT"),
-            TokenKind::Return => write!(f, "RETURN"),
-            TokenKind::Super => write!(f, "SUPER"),
-            TokenKind::This => write!(f, "THIS"),
-            TokenKind::True => write!(f, "TRUE"),
-            TokenKind::Var => write!(f, "VAR"),
-            TokenKind::While => write!(f, "WHILE"),
+            TokenKind::And => write!(f, "AND {origin} null"),
+            TokenKind::Class => write!(f, "CLASS {origin} null"),
+            TokenKind::Else => write!(f, "ELSE {origin} null"),
+            TokenKind::False => write!(f, "FALSE {origin} null"),
+            TokenKind::For => write!(f, "FOR {origin} null"),
+            TokenKind::Fun => write!(f, "FUN {origin} null"),
+            TokenKind::If => write!(f, "IF {origin} null"),
+            TokenKind::Nil => write!(f, "NIL {origin} null"),
+            TokenKind::Or => write!(f, "OR {origin} null"),
+            TokenKind::Print => write!(f, "PRINT {origin} null"),
+            TokenKind::Return => write!(f, "RETURN {origin} null"),
+            TokenKind::Super => write!(f, "SUPER {origin} null"),
+            TokenKind::This => write!(f, "THIS {origin} null"),
+            TokenKind::True => write!(f, "TRUE {origin} null"),
+            TokenKind::Var => write!(f, "VAR {origin} null"),
+            TokenKind::While => write!(f, "WHILE {origin} null"),
         }
     }
 }
@@ -148,6 +153,7 @@ pub struct Lexer<'de> {
     whole: &'de str,
     rest: &'de str,
     byte: usize,
+    peeked: Option<Result<Token<'de>, miette::Error>>,
 }
 
 impl<'de> Lexer<'de> {
@@ -156,17 +162,64 @@ impl<'de> Lexer<'de> {
             whole: input,
             rest: input,
             byte: 0,
+            peeked: None,
         }
+    }
+}
+
+impl<'de> Lexer<'de> {
+    pub fn expect(
+        &mut self,
+        expected: TokenKind,
+        unexpected: &str,
+    ) -> Result<Token<'de>, miette::Error> {
+        self.expect_where(|next| next.kind == expected, unexpected)
+    }
+
+    pub fn expect_where(
+        &mut self,
+        mut check: impl FnMut(&Token<'de>) -> bool,
+        unexpected: &str,
+    ) -> Result<Token<'de>, miette::Error> {
+        match self.next() {
+            Some(Ok(token)) if check(&token) => Ok(token),
+            Some(Ok(token)) => Err(miette::miette! {
+                labels = vec![
+                    LabeledSpan::at(token.offset..token.offset + token.origin.len(), "here"),
+                ],
+                help = format!("Expected {token:?}"),
+                "{unexpected}",
+            }
+            .with_source_code(self.whole.to_string())),
+            Some(Err(e)) => Err(e),
+            None => Err(Eof.into()),
+        }
+    }
+
+    pub fn peek(&mut self) -> Option<&Result<Token<'de>, miette::Error>> {
+        if self.peeked.is_some() {
+            return self.peeked.as_ref();
+        }
+
+        self.peeked = self.next();
+        self.peeked.as_ref()
     }
 }
 
 impl<'de> Iterator for Lexer<'de> {
     type Item = Result<Token<'de>, Error>;
 
+    /// Once the iterator returns `Err`, it will only return `None`.
     fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next) = self.peeked.take() {
+            return Some(next);
+        }
+
         loop {
+            // NOTE: this must be in the loop for the indices to match up with c_onwards
             let mut chars = self.rest.chars();
             let c = chars.next()?;
+            let c_at = self.byte;
             let c_str = &self.rest[..c.len_utf8()];
             let c_onwards = self.rest;
             self.rest = chars.as_str();
@@ -183,6 +236,7 @@ impl<'de> Iterator for Lexer<'de> {
             let just = move |kind: TokenKind| {
                 Some(Ok(Token {
                     kind,
+                    offset: c_at,
                     origin: c_str,
                 }))
             };
@@ -204,16 +258,16 @@ impl<'de> Iterator for Lexer<'de> {
                 '!' => Started::IfEqualElse(TokenKind::BangEqual, TokenKind::Bang),
                 '=' => Started::IfEqualElse(TokenKind::EqualEqual, TokenKind::Equal),
                 '"' => Started::String,
-                c if c.is_whitespace() => continue,
                 '0'..='9' => Started::Number,
                 'a'..='z' | 'A'..='Z' | '_' => Started::Ident,
+                c if c.is_whitespace() => continue,
                 c => {
                     return Some(Err(SingleTokenError {
                         src: self.whole.to_string(),
                         token: c,
                         err_span: SourceSpan::from(self.byte - c.len_utf8()..self.byte),
                     }
-                    .into()))
+                    .into()));
                 }
             };
 
@@ -225,6 +279,7 @@ impl<'de> Iterator for Lexer<'de> {
                         self.rest = &self.rest[end + 1..];
                         Some(Ok(Token {
                             origin: literal,
+                            offset: c_at,
                             kind: TokenKind::String,
                         }))
                     } else {
@@ -233,6 +288,7 @@ impl<'de> Iterator for Lexer<'de> {
                             err_span: SourceSpan::from(self.byte - c.len_utf8()..self.whole.len()),
                         };
 
+                        // swallow the remainder of input as being a string
                         self.byte += self.rest.len();
                         self.rest = &self.rest[self.rest.len()..];
 
@@ -249,6 +305,7 @@ impl<'de> Iterator for Lexer<'de> {
                     } else {
                         Some(Ok(Token {
                             origin: c_str,
+                            offset: c_at,
                             kind: TokenKind::Slash,
                         }))
                     }
@@ -285,6 +342,7 @@ impl<'de> Iterator for Lexer<'de> {
 
                     return Some(Ok(Token {
                         origin: literal,
+                        offset: c_at,
                         kind,
                     }));
                 }
@@ -297,16 +355,15 @@ impl<'de> Iterator for Lexer<'de> {
                     let mut dotted = literal.splitn(3, '.');
                     match (dotted.next(), dotted.next(), dotted.next()) {
                         (Some(one), Some(two), Some(_)) => {
-                            literal = &literal[..one.len() + 1 + two.len()]
+                            literal = &literal[..one.len() + 1 + two.len()];
                         }
                         (Some(one), Some(two), None) if two.is_empty() => {
-                            literal = &literal[..one.len()]
+                            literal = &literal[..one.len()];
                         }
                         _ => {
                             // leave literal as-is
                         }
                     }
-
                     let extra_bytes = literal.len() - c.len_utf8();
                     self.byte += extra_bytes;
                     self.rest = &self.rest[extra_bytes..];
@@ -314,19 +371,18 @@ impl<'de> Iterator for Lexer<'de> {
                     let n = match literal.parse() {
                         Ok(n) => n,
                         Err(e) => {
-                            return Some(Err(miette::miette!(
-                                labels = vec![LabeledSpan::at(
-                                    self.byte - literal.len()..self.byte,
-                                    "this numeric literal"
-                                )],
-                                "{e}"
-                            )
-                            .with_source_code(self.whole.to_string())));
+                            return Some(Err(miette::miette! {
+                                labels = vec![
+                                    LabeledSpan::at(self.byte - literal.len()..self.byte, "this numeric literal"),
+                                ],
+                                "{e}",
+                            }.with_source_code(self.whole.to_string())));
                         }
                     };
 
                     return Some(Ok(Token {
                         origin: literal,
+                        offset: c_at,
                         kind: TokenKind::Number(n),
                     }));
                 }
@@ -340,11 +396,13 @@ impl<'de> Iterator for Lexer<'de> {
                         self.byte += 1;
                         Some(Ok(Token {
                             origin: span,
+                            offset: c_at,
                             kind: yes,
                         }))
                     } else {
                         Some(Ok(Token {
                             origin: c_str,
+                            offset: c_at,
                             kind: no,
                         }))
                     }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use miette::{IntoDiagnostic, WrapErr};
 use std::fs;
 use std::path::PathBuf;
 
-use rusty::*;
+use rusty as imp;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -15,24 +15,25 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Commands {
     Tokenize { filename: PathBuf },
+    Parse { filename: PathBuf },
+    Run { filename: PathBuf },
 }
 
 fn main() -> miette::Result<()> {
     let args = Args::parse();
     let mut any_cc_err = false;
-
     match args.command {
         Commands::Tokenize { filename } => {
             let file_contents = fs::read_to_string(&filename)
                 .into_diagnostic()
                 .wrap_err_with(|| format!("reading '{}' failed", filename.display()))?;
 
-            for token in Lexer::new(&file_contents) {
+            for token in imp::Lexer::new(&file_contents) {
                 let token = match token {
                     Ok(t) => t,
                     Err(e) => {
                         eprintln!("{e:?}");
-                        if let Some(unrecognized) = e.downcast_ref::<lex::SingleTokenError>() {
+                        if let Some(unrecognized) = e.downcast_ref::<imp::lex::SingleTokenError>() {
                             any_cc_err = true;
                             eprintln!(
                                 "[line {}] Error: Unexpected character: {}",
@@ -40,16 +41,33 @@ fn main() -> miette::Result<()> {
                                 unrecognized.token
                             );
                         } else if let Some(unterminated) =
-                            e.downcast_ref::<lex::StringTerminationError>()
+                            e.downcast_ref::<imp::lex::StringTerminationError>()
                         {
                             any_cc_err = true;
-                            eprintln!("[line {}] Error: Unterminated string.", unterminated.line());
+                            eprintln!("[line {}] Error: Unterminated string.", unterminated.line(),);
                         }
                         continue;
                     }
                 };
                 println!("{token}");
             }
+            println!("EOF  null");
+        }
+        Commands::Parse { filename } => {
+            let file_contents = fs::read_to_string(&filename)
+                .into_diagnostic()
+                .wrap_err_with(|| format!("reading '{}' failed", filename.display()))?;
+
+            let parser = imp::Parser::new(&file_contents);
+            println!("{}", parser.parse_expression().unwrap());
+        }
+        Commands::Run { filename } => {
+            let file_contents = fs::read_to_string(&filename)
+                .into_diagnostic()
+                .wrap_err_with(|| format!("reading '{}' failed", filename.display()))?;
+
+            let parser = imp::Parser::new(&file_contents);
+            println!("{}", parser.parse().unwrap());
         }
     }
 
